45ac9ccd316fbd79b8ede79702861d292744e57e
LuK1337 <priv.luk@gmail.com>
2017-10-05 11:53:07 +0200

    Implement basic steam api client class

---
+++ app/SteamApiClient.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+use GuzzleHttp\Client;
+
+class SteamApiClient
+{
+    private $apiKey;
+    private $apiClient;
+
+    function __construct() {
+        $this->apiKey = env('STEAM_API_KEY', '');
+        $this->apiClient = new Client([
+            'base_uri' => 'http://api.steampowered.com',
+        ]);
+    }
+
+    function GetPlayerBans($steamIds) {
+        $request = $this->apiClient->request('GET', 'ISteamUser/GetPlayerBans/v1', [
+            'query' => [
+                'key' => $this->apiKey,
+                'steamids' => $steamIds,
+            ],
+        ]);
+        $json = json_decode($request->getBody(), true);
+        return $json['players'];
+    }
+
+    function getPlayerSummaries($steamIds) {
+        $request = $this->apiClient->request('GET', 'ISteamUser/GetPlayerSummaries/v0002', [
+            'query' => [
+                'key' => $this->apiKey,
+                'steamids' => $steamIds,
+            ],
+        ]);
+        $json = json_decode($request->getBody(), true);
+        return $json['response']['players'];
+    }
+
+    function resolveVanityURL($vanityUrl) {
+        $request = $this->apiClient->request('GET', 'ISteamUser/ResolveVanityURL/v0001', [
+            'query' => [
+                'key' => $this->apiKey,
+                'vanityurl' => $vanityUrl,
+            ],
+        ]);
+        $json = json_decode($request->getBody(), true);
+        return $json['response'];
+    }
+}
