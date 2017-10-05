86d9d96c289e7e4e79ba9bc92e30c572e17d705f
LuK1337 <priv.luk@gmail.com>
2017-10-05 20:31:01 +0200

    Run v:fetch-new-bans every 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('v:fetch-new-bans')
-            ->everyTenMinutes();
+            ->everyFiveMinutes();
     }
 
     /**
