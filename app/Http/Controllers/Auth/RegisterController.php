3a361bf253af6875a2a1c056cd00dc24a0e3053e
LuK1337 <priv.luk@gmail.com>
2017-10-05 15:17:25 +0200

    Fix register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,7 +62,6 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
