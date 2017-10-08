555c6419a3976ff63725c5e4cf236bc4ac1a66d9
LuK1337 <priv.luk@gmail.com>
2017-10-09 01:31:05 +0200

    Use HTML table in new_bans template
    * Figuring out how to escape names
      in markdown isn't worth my time.

--- resources/views/mail/new_bans.blade.php
+++ resources/views/mail/new_bans.blade.php
@@ -3,11 +3,28 @@
 
 You are receiving this email because someone just got banned from list 〜 {{ $list->name }}!
 
-@component('mail::table')
-| Avatar                          | Name                                                                                | Game bans                           | VAC bans                           |
-|:-------------------------------:| ----------------------------------------------------------------------------------- |:-----------------------------------:|:----------------------------------:|
-@foreach ($accounts as $account)
-| ![avatar]({{$account->avatar}}) | [{{ $account->name }}](https://steamcommunity.com/profiles/{{ $account->steamid }}) | {{ $account->number_of_game_bans }} | {{ $account->number_of_vac_bans }}
-@endforeach
+@component('mail::html')
+<div class="table">
+    <table>
+        <thead>
+        <tr>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Game bans</th>
+            <th>VAC bans</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($accounts as $account)
+                <tr>
+                    <td style="text-align: center"><img src="{{ $account->avatar }}" /></td>
+                    <td><a href="https://steamcommunity.com/profiles/{{ $account->steamid }}">{{ $account->name }}</a></td>
+                    <td style="text-align: center">{{ $account->number_of_game_bans }}</td>
+                    <td style="text-align: center">{{ $account->number_of_vac_bans }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endcomponent
 @endcomponent
