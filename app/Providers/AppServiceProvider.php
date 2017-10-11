72f1a598caefff2e21983d0aff5dba62a0994de9
LuK1337 <priv.luk@gmail.com>
2017-10-12 00:35:40 +0200

    Add escape_markdown blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Blade::directive('escape_markdown', function ($expression) {
+            return "<?php
+                \$specialCharacters = ['\\\\', '`', '*', '_', '{', '}', '[', ']', '(', ')', '#', '+', '-', '!', '|'];
+                \$specialCharactersEscaped = ['\\\\\\\\', '\\\\`', '\\\\*', '\\\\_', '\\\\{', '\\\\}', '\\\\[', '\\\\]', '\\\\(', '\\\\)', '\\\\#', '\\\\+', '\\\\-', '\\\\!', '\\\\|'];
+                echo e(str_replace(\$specialCharacters, \$specialCharactersEscaped, {$expression}));
+                ?>";
+        });
     }
 
     /**
