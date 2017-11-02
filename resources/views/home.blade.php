5029f213af9c9f8f65b78ea30af546a8274e7a55
LuK1337 <priv.luk@gmail.com>
2017-11-02 02:44:57 +0100

    Add simple home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,22 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+            <h2 class="page-title">Home</h2>
 
+            <p>Welcome to V.</p>
+
+            <p>By creating an account here you'll be able to:</p>
+            <ul>
+                <li>Add suspicious players to your list(s).</li>
+                <li>Subscribe to public / unlisted list(s).</li>
+                <li>Get notified whenever somebody gets banned.</li>
+            </ul>
+
+            <p>As an unregistered user you'll be able to:</p>
+            <ul>
+                <li>View public / unlisted lists.</li>
+                <li>View list of latest bans.</li>
+            </ul>
         </div>
     </div>
 </div>
