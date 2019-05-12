476e23a161d59fac35355ac938fc5e7acba5554c
LuK1337 <priv.luk@gmail.com>
2019-05-12 20:37:02 +0200

    Sync with Laravel 5.8

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -29,7 +28,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
 
 $(function () {
