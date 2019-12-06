9ede1a366b71baf4c78ac9f58aeba80ee0acd093
LuK1337 <priv.luk@gmail.com>
2019-12-06 15:20:06 +0100

    Rounded avatars lol

--- resources/views/latest-bans.blade.php
+++ resources/views/latest-bans.blade.php
@@ -24,8 +24,8 @@
                             <tr class="valign-middle">
                                 <th scope="row">{{ $loop->iteration + (($accounts->currentPage() - 1) * $accounts->perPage()) }}</th>
                                 <td>
-                                    <a href="https://steamcommunity.com/profiles/{{ $account->steamid }}">
-                                        <img class="is-block" src="{{ $account->avatar }}"/>
+                                    <a href="https://steamcommunity.com/profiles/{{ $account->steamid }}" class="image is-32x32">
+                                        <img class="is-block is-rounded" src="{{ $account->avatar }}"/>
                                     </a>
                                 </td>
                                 <td>
