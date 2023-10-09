3c7c7444c81f4c57a23058e14eaf185ce3cd3c27
LuK1337 <priv.luk@gmail.com>
2023-10-09 16:25:56 +0200

    Make v:fetch-new-bans run every 30m instead of every 5m

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('v:fetch-new-bans')
-            ->everyFiveMinutes();
+            ->everyThirtyMinutes();
     }
 
     /**
